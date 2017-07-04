c6bbbb6b58ad364b8d53a04299baa1edb57e9396
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-07-04 11:08:04 +0200

    Tp 01 java

---
+++ Bonjour.java
@@ -0,0 +1,13 @@
+ /**
+            @author Jerome Samson
+          */
+public class Bonjour{
+    public static void main (String[] args){
+        // Récupération du nom passer en paramètre
+        /*
+        Affichage du nom 
+         */
+        
+        System.out.println("Bonjour " + args[0]);
+    }
+}
